62d95309ba39f1d496e13fb26b2d0d7e5eb414db
Chris Willoughby <chrrrisw@gmail.com>
2015-10-01 08:14:16 +0930

    testing stuff

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,17 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+QUnit.test( "init test", function( assert ) {
+    // Get the key_file
+    $.get('/__key_file__', function(data) {
+        Trove.init(data);
+        search = new Trove.Search({
+            done: function(s) {
+                console.log(s._last_search);
+                assert.ok( s.__last_search.zone == 'all', "Default zone passed");
+            }
+        });
+        search.query({
+            terms: "john smith"
+        });
+    });
+
+    assert.ok( 1 == "1", "Passed!" );
 });
